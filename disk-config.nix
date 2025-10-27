7aae5ddd54d1cc9086149381f52537baff5073c1
Litengat <Litengut@icloud.com>
2025-10-28 00:05:05 +0100

    Refactor disk configuration for clarity and consistency; format user settings for improved readability

--- disk-config.nix
+++ disk-config.nix
@@ -3,76 +3,76 @@
 {
   disko.devices = {
     disk = {
+      main = {
+        device = lib.mkDefault "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
+            };
+            esp = {
+              name = "ESP";
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              name = "root";
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
+            };
+          };
+        };
+      };
+
       disk1 = {
-      device = lib.mkDefault "/dev/sda";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+        type = "disk";
+        device = "/dev/sdc";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "data";
+              };
             };
           };
-          root = {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "lvm_pv";
-              vg = "pool";
+        };
+      };
+      disk2 = {
+        type = "disk";
+        device = "/dev/sdc";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "data";
+              };
             };
           };
         };
       };
     };
+    # zpool = {
 
-    disk1 = {
-      type = "disk";
-      device="/dev/sdc";
-      content = {   
-        type = "gpt";
-        partitions = {
-          zfs = {
-            size = "100%";
-            content = {
-              type = "zfs";
-              pool = "data";
-              };
-            };ƒ
-          }
-        };
-      };
-    disk2 = {
-      type = "disk";
-      device="/dev/sdc";
-      content = {   
-        type = "gpt";
-        partitions = {
-          zfs = {
-            size = "100%";
-            content = {
-              type = "zfs";
-              pool = "data";
-              };
-            };
-          }
-        };
-      };
-    };
-    zpool = {
+    # }
 
-    }
-    
     lvm_vg = {
       pool = {
         type = "lvm_vg";
